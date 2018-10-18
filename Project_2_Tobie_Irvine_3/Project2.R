fdd0a31522f6a310cc04f8fd9c56730d6ac0e65d
atirvine88 <atirvine88@gmail.com>
2018-10-18 17:59:17 -0400

    More changes; trying to get API for map to work. Added geojson shape file (this is static)

--- Project_2_Tobie_Irvine_3/Project2.R
+++ Project_2_Tobie_Irvine_3/Project2.R
@@ -14,7 +14,7 @@ library(leaflet)
 library(rgdal)
 library(leaflet.extras)
 
-## Creation of ckan function for csv data
+## Creation of ckan function for csv data (see Homework 4)
 ckanSQL <- function(url) {
   # Make the Request
   r <- RETRY("GET", URLencode(url))
@@ -35,27 +35,28 @@ ckanMap <- function(url) {
   # Basic gsub to make NA's consistent with R
   json <- gsub('NaN', 'NA', c, perl = TRUE)
   # Create Dataframe
-  data.frame(jsonlite::fromJSON(json)$result$records)
+  readOGR(json)
 }
-# Unique values for Resource Field
+## Unique values for Resource Field
 ckanUnique <- function(id, field) {
   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
   c(ckanSQL(URLencode(url)))
 }
-#Create choices for fields of Race and Gender
+
+##Create choices for fields of Race and Gender
 # gender_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Gender")$Gender)
 # race_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Race")$Race)
 
 #Relabel choices of gender and race (input in user interface)
 # Moved gender choice refactoring to the reactive function
 
- 
+##UI for Shiny App 
  sidebar <- dashboardSidebar(
    sidebarMenu(
      id = "tabs",
      menuItem("Charts", icon = icon("bar-chart"), tabName = "chart"),
      menuItem("Data Table", icon = icon("table"), tabName = "datatable"),
-     menuItem("Map", icon = icon("map"), tabName = "map")
+     menuItem("Map", icon = icon("map"), tabName = "map"),
      radioButtons(),
      selectInput(),
      selectInput(),
@@ -84,14 +85,12 @@ ckanUnique <- function(id, field) {
                        )
              
    ) 
- )
- 
- 
+ ))
  # Define UI for shiny dashboard
  ui <- dashboardPage(header, sidebar, body)
  
  # Define server logic
- server <- function(input, output) {
+ server <- function(input, output, session) {
    #Reactive Element for Charts and Datatable 
    df.filter <- reactive ({
      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20=%20%27", 
@@ -104,19 +103,14 @@ ckanUnique <- function(id, field) {
      print(colnames(clean.data))
      return(clean.data)
    })
+   
    #Reactive Element for Map
    df.filter2 <- reactive ({
-     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20=%20%27", 
+     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22 7f5da957-01b5-4187-a842-3f215d30d7e8 %22%20WHERE%20%22Gender%22%20=%20%27", 
                    input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
      # use ckan on url and make clean data
-     clean.data <- ckanSQL(url) %>% na.omit() %>%
-       mutate(Gender = plyr::revalue(Gender,  c("F" = 'Female',  "M" = 'Male'))
-       ) %>%
-       mutate(Race = plyr::revalue(Race,  c("A" = 'Asian',  "B" = 'Black', "H" = 'Hispanic', "I" = 'Indian', "U" = 'Unknown', "W" = 'White', "x" = 'Unreported')))
-     print(colnames(clean.data))
+     clean.data <- ckanmap(url) 
      return(clean.data)
-     
-     
    })
   
    
@@ -141,10 +135,10 @@ ckanUnique <- function(id, field) {
    #Create Interactive Map
    output$mymap <- render$leaflet({
      
-     readOGR("PaRailLines2018_07.geojson")
+   trial <- readOGR("2010_Census_Tracts.geojson")
    
    #Create map
-   leaflet(data = pa.rails) %>%
+   leaflet(data = trial) %>%
      addProviderTiles("OpenMapSurfer.Roads", options = providerTileOptions(noWrap = TRUE), group = "Default") %>%
      addProviderTiles("Esri.DeLorme", options = providerTileOptions(noWrap = TRUE), group = "Topographical") %>%
      addProviderTiles(providers$Esri.WorldImagery, options = providerTileOptions(noWrap = TRUE), group = "World") %>%
