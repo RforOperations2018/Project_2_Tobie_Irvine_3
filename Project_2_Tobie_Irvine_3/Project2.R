ae6bd82cd089e0c161a9dbcf1ec508b135533b47
atirvine88 <atirvine88@gmail.com>
2018-10-18 15:14:19 -0400

    Creating Structure of App

--- Project_2_Tobie_Irvine_3/Project2.R
+++ Project_2_Tobie_Irvine_3/Project2.R
@@ -11,7 +11,7 @@ library(jsonlite)
 library(plotly)
 library(htmltools)
 
-## Creation of
+## Creation of ckan function
 ckanSQL <- function(url) {
   # Make the Request
   r <- RETRY("GET", URLencode(url))
@@ -28,89 +28,114 @@ ckanUnique <- function(id, field) {
   c(ckanSQL(URLencode(url)))
 }
 #Create choices for fields of Race and Gender
-gender_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Gender")$Gender)
-race_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Race")$Race)
+# gender_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Gender")$Gender)
+# race_choices <- sort(ckanUnique("7f5da957-01b5-4187-a842-3f215d30d7e8", "Race")$Race)
 
 #Relabel choices of gender and race (input in user interface)
 # Moved gender choice refactoring to the reactive function
- race_choices <- recode(race_choices, A = "Asian", B = "Black", H = "Hispanic", I = "Indian", U = "Unknown", W = "White", x = "Unreported")
 
-# Define UI for application using a fluid page layout
-ui <- fluidPage(
-# Layout of Basic Input          
-  titlePanel("Allegheny County Jail Bookings for September "),
-      tabsetPanel(
-        #1st Panel for Gender and Race Information and Refresh Button
-        tabPanel("Basic Information",
-                  fluidRow(
-                    column(4,
-
-                           wellPanel(radioButtons("race", label = "Race", 
-                                                 choiceNames = c('Asian' , 'Black','Hispanic','Indian','Unknown','White','Unreported'),
-                                                 choiceValues = c("A", "B", "H", "I", "U", "W", "x")
-                                                 )),
-                           wellPanel(radioButtons("gender", label = "Options", choiceNames = c("Female", "Male"), choiceValues = c("F", "M"))),
-                           wellPanel(actionButton("click", "Click to See What Happens"))),
-                   column(8, wellPanel( plotlyOutput("race.info")), 
-                             wellPanel(plotlyOutput("gender.info"))))),
-         #2nd Panel for Download Button and Data Table
-         tabPanel("Downloads and Table",
-                   fluidRow(column(4, wellPanel(downloadButton("new.download", label = "Download New File"))),
-                            column(8, wellPanel(dataTableOutput("table", width = "80%")))))
-      ))
-
-# Define server logic required to draw two bar graphs, a data table, and a download button
-server <- function(input, output, session = session) {
-# create reactive element  
-df.filter <- reactive ({
-   url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20=%20%27", 
-                input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
+ 
+ sidebar <- dashboardSidebar(
+   sidebarMenu(
+     id = "tabs",
+     menuItem("Charts", icon = icon("bar-chart"), tabName = "plot"),
+     menuItem("Death Data Table", icon = icon("table"), tabName = "datatable"),
+     radioButtons("cause",
+                  "Pick a cause of death: ",
+                  choices = unique(State.Death.data$Cause.Name),
+                  #multiple = FALSE,
+                  #selectize = TRUE,
+                  selected = "Alzheimer's disease"
+     ),
+     selectInput("state",
+                 "State:",
+                 choices = sort(unique(State.Death.data$State)),
+                 multiple = TRUE,
+                 selectize = TRUE,
+                 selected = "Alabama"),
+     selectInput("year", 
+                 "Pick a Year: ",
+                 choices = sort(unique(State.Death.data$Year)),
+                 multiple = FALSE,
+                 selectize = TRUE,
+                 selected = 1999
+     )
+   )
+ )
+ 
+ body <- dashboardBody(
+   tabItems(
+     tabItem("plot",
+             # Don't be afraid to delete stuff. Once you commit it you can always go back and grab it from Git History!
+             # fluidRow(
+             #   infoBoxOutput("mass"),
+             #   valueBoxOutput("height")
+             # ),
+             fluidRow(
+               tabBox(title = "Plot",
+                      width = 12,
+                      tabPanel("Cumulative Deaths", plotOutput("plot1")),
+                      tabPanel("Death Rate", plotOutput("plot2")),
+                      tabPanel("Time Plot", plotOutput("plot3")))
+             )
+     ),
+     tabItem("datatable",
+             fluidPage(
+               box(title = "Selected Character Stats", DT::dataTableOutput("datatable"), width = 12))
+     )
+   ) 
+ )
+ 
+ 
+ # Define UI for shiny dashboard
+ ui <- dashboardPage(header, sidebar, body)
+ 
+ # Define server logic
+ server <- function(input, output) {
+   #Reactive Elements 
+   df.filter <- reactive ({
+     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20=%20%27", 
+                   input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
      # use ckan on url and make clean data
-  clean.data <- ckanSQL(url) %>% na.omit() %>%
-   mutate(Gender = plyr::revalue(Gender,  c("F" = 'Female',  "M" = 'Male'))
-          ) %>%
-  mutate(Race = plyr::revalue(Race,  c("A" = 'Asian',  "B" = 'Black', "H" = 'Hispanic', "I" = 'Indian', "U" = 'Unknown', "W" = 'White', "x" = 'Unreported')))
-  print(colnames(clean.data))
-  return(clean.data)
+     clean.data <- ckanSQL(url) %>% na.omit() %>%
+       mutate(Gender = plyr::revalue(Gender,  c("F" = 'Female',  "M" = 'Male'))
+       ) %>%
+       mutate(Race = plyr::revalue(Race,  c("A" = 'Asian',  "B" = 'Black', "H" = 'Hispanic', "I" = 'Indian', "U" = 'Unknown', "W" = 'White', "x" = 'Unreported')))
+     print(colnames(clean.data))
+     return(clean.data)
    })
-#Create Interactive Race and Gender Plot  
-   output$race.info <- renderPlotly({
-    df2 <- df.filter()
-    # ggplots are easier to read when each plus starts a new line.
-    ggplotly(ggplot(data = df2, aes(x = Race , fill = Race)) + 
-               geom_bar() + 
-               ggtitle("Arrests by Race and Gender for September") +
-               scale_fill_manual(values = c("Asian" = "green", "White" = "orange", 
-                                            "Black" = "purple", "Hispanic" = "yellow",
-                                            "Indian" = "pink", "Unreported" = "red",
-                                            "Unknown" = "grey")) +
-               ylab("Total"))
-    }) 
-#Create Interactive Gender Plot
-  output$gender.info <- renderPlotly({
-    df1 <- df.filter()
-    ggplotly(ggplot(data = df1, aes(x = Gender, fill = Gender)) + 
-      geom_bar()+ ggtitle("Arrests by Sex for September") +  scale_fill_manual (values = c("Female" = "blue", "Male" = "brown")) + 
-        ylab("Total"))})
-#Create Download Button that allows people to download info
-  output$new.download <- downloadHandler(
-    filename = function(){ 
-      paste("new.download", Sys.Date(), ".csv", sep = "" )},
-    content = function(file) {
-      write.csv(df.filter(), file, row.names = FALSE)
-    })
-#Create table  
-  output$table <- renderDataTable({
-    functioning.table <- df.filter()
-    subset(functioning.table, select = c(Date, Gender, Race, Age.at.Booking, Current.Age))
-  })
-#Create Refresh Button   
-  observeEvent(input$click, {
-    updateRadioButtons(session, "race", selected = 'A')
-    updateRadioButtons(session, "gender", selected = 'F')
-    showNotification("You have successfully reset the filters for race", type = "message")
-      })
-}
-# Run the application 
-shinyApp(ui = ui, server = server)
+   
+  
+   
+   #Create Chart 1  
+   output$plot1 <- renderPlot({
+     df2 <- deathInput()
+     ggplot(df2, aes(x = State, y = Deaths, color = State)) + 
+       geom_bar(stat = "identity") + 
+       ggtitle("Total Deaths per Accident per Year") + 
+       ylab("Total Deaths") +
+       theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   })
+   
+   #Create Chart 2
+   output$plot2 <-  renderPlot({
+     df3 <- deathInput()
+     ggplot(df3, aes(x = State, y = Age.adjusted.Death.Rate, color = State)) + 
+       geom_bar(stat = "identity") + 
+       ggtitle("Death Rate (per 100,000) per Accident per Year") + 
+       ylab("Adjusted Death Rate")
+   })
+   #Create Interactive Map
+   output$map <-  renderPlot({
+     df4 <- 
+     
+   })
+   # Create a Data Table
+   output$datatable <- DT::renderDataTable({
+     subset(deathInput(), select = c(State, Cause.Name, Deaths, Age.adjusted.Death.Rate))
+   })
+ }
+ 
+ # Run the application 
+ shinyApp(ui = ui, server = server)
 
