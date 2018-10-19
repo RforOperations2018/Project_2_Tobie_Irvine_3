c786368e0d05941c07ede29a6b5dacd1d39ae1d1
atirvine88 <atirvine88@gmail.com>
2018-10-18 23:08:53 -0400

    Revert "Trying to get one input to work. API does work but now trying to add Polygons. Frustration ensues"
    This reverts commit ffe1ff34d622ac216832b663231e4e5a8ef52689.

--- Project_2_Tobie_Irvine_3/Project2.R
+++ Project_2_Tobie_Irvine_3/Project2.R
@@ -13,8 +13,6 @@ library(htmltools)
 library(leaflet)
 library(rgdal)
 library(leaflet.extras)
-library(shinydashboard)
-
 
 ## Creation of ckan function for csv data (see Homework 4)
 ckanSQL <- function(url) {
@@ -52,17 +50,17 @@ ckanUnique <- function(id, field) {
 #Relabel choices of gender and race (input in user interface)
 # Moved gender choice refactoring to the reactive function
 
-
-##UI for Shiny App
-header <- dashboardHeader(title = "hahahaha")
+##UI for Shiny App 
  sidebar <- dashboardSidebar(
-  
    sidebarMenu(
      id = "tabs",
      menuItem("Charts", icon = icon("bar-chart"), tabName = "chart"),
      menuItem("Data Table", icon = icon("table"), tabName = "datatable"),
      menuItem("Map", icon = icon("map"), tabName = "map"),
-     selectInput(inputId = "neighborhood", label = "Pick a neighborhood", choices = c("Bloomington", "Problem"))
+     radioButtons(),
+     selectInput(),
+     selectInput(),
+     sliderInput()
    )
  )
  
@@ -92,67 +90,68 @@ header <- dashboardHeader(title = "hahahaha")
  ui <- dashboardPage(header, sidebar, body)
  
  # Define server logic
- server <- function(input, output) {
-  
-   #Reactive Element for Map
-   df.filter2 <- reactive ({
-     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%228d76ac6b-5ae8-4428-82a4-043130d17b02%22%20WHERE%20%22neighborhood%22%20=%20%27Bloomfield%27%20")
+ server <- function(input, output, session) {
+   #Reactive Element for Charts and Datatable 
+   df.filter <- reactive ({
+     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%227f5da957-01b5-4187-a842-3f215d30d7e8%22%20WHERE%20%22Gender%22%20=%20%27", 
+                   input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
      # use ckan on url and make clean data
-     clean.data <- ckanSQL(url)
+     clean.data <- ckanSQL(url) %>% na.omit() %>%
+       mutate(Gender = plyr::revalue(Gender,  c("F" = 'Female',  "M" = 'Male'))
+       ) %>%
+       mutate(Race = plyr::revalue(Race,  c("A" = 'Asian',  "B" = 'Black', "H" = 'Hispanic', "I" = 'Indian', "U" = 'Unknown', "W" = 'White', "x" = 'Unreported')))
      print(colnames(clean.data))
      return(clean.data)
    })
-   #Create Interactive Map
-   output$mymap <- renderLeaflet({
-     
-   trial <- readOGR("2010_Census_Tracts.geojson") %>% na.omit
-   fires <- df.filter2()
-   #Create map
-   leaflet() %>%
-     addProviderTiles("OpenStreetMap.France", options = providerTileOptions(noWrap = TRUE), group = "Default") %>%
-     # addProviderTiles("Esri.DeLorme", options = providerTileOptions(noWrap = TRUE), group = "Topographical") %>%
-     # addProviderTiles(providers$Esri.WorldImagery, options = providerTileOptions(noWrap = TRUE), group = "World") %>%
-     # # Layers Control
-     # addLayersControl(
-     #   baseGroups = c("Default", "Topographical", "World"),
-     #   options = layersControlOptions(collapsed = FALSE)
-     # )  %>%
-     #addPolygons(data = trial) %>%
-     addCircleMarkers(data = fires, lng = ~longitude, lat = ~latitude, radius = 1.5)
-   })
    
-   #Reactive Element for Charts and Datatable 
-   # df.filter <- reactive ({
-   #   url <- paste0()
-   #   # use ckan on url and make clean data
-   #   clean.data <- ckanSQL(url) %>% na.omit()
-   #    
-   #   print(colnames(clean.data))
-   #   return(clean.data)
-   # })
-   # Create a Data Table
-   # output$datatable <- DT::renderDataTable({
-   #   subset(deathInput(), select = c(State, Cause.Name, Deaths, Age.adjusted.Death.Rate))
-   # })
+   #Reactive Element for Map
+   df.filter2 <- reactive ({
+     url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22 7f5da957-01b5-4187-a842-3f215d30d7e8 %22%20WHERE%20%22Gender%22%20=%20%27", 
+                   input$gender,"%27%20AND%20%22Race%22%20%3D%20%27", input$race, "%27%20")
+     # use ckan on url and make clean data
+     clean.data <- ckanmap(url) 
+     return(clean.data)
+   })
+  
    
    #Create Chart 1  
-   # output$plot1 <- renderPlot({
-   #   df2 <- deathInput()
-   #   ggplot(df2, aes(x = State, y = Deaths, color = State)) + 
-   #     geom_bar(stat = "identity") + 
-   #     ggtitle("Total Deaths per Accident per Year") + 
-   #     ylab("Total Deaths") +
-   #     theme(axis.text.x = element_text(angle = 90, hjust = 1))
-   # })
-   # 
-   # #Create Chart 2
-   # output$plot2 <-  renderPlot({
-   #   df3 <- deathInput()
-   #   ggplot(df3, aes(x = State, y = Age.adjusted.Death.Rate, color = State)) + 
-   #     geom_bar(stat = "identity") + 
-   #     ggtitle("Death Rate (per 100,000) per Accident per Year") + 
-   #     ylab("Adjusted Death Rate")
-   # })
+   output$plot1 <- renderPlot({
+     df2 <- deathInput()
+     ggplot(df2, aes(x = State, y = Deaths, color = State)) + 
+       geom_bar(stat = "identity") + 
+       ggtitle("Total Deaths per Accident per Year") + 
+       ylab("Total Deaths") +
+       theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   })
+   
+   #Create Chart 2
+   output$plot2 <-  renderPlot({
+     df3 <- deathInput()
+     ggplot(df3, aes(x = State, y = Age.adjusted.Death.Rate, color = State)) + 
+       geom_bar(stat = "identity") + 
+       ggtitle("Death Rate (per 100,000) per Accident per Year") + 
+       ylab("Adjusted Death Rate")
+   })
+   #Create Interactive Map
+   output$mymap <- render$leaflet({
+     
+   trial <- readOGR("2010_Census_Tracts.geojson")
+   
+   #Create map
+   leaflet(data = trial) %>%
+     addProviderTiles("OpenMapSurfer.Roads", options = providerTileOptions(noWrap = TRUE), group = "Default") %>%
+     addProviderTiles("Esri.DeLorme", options = providerTileOptions(noWrap = TRUE), group = "Topographical") %>%
+     addProviderTiles(providers$Esri.WorldImagery, options = providerTileOptions(noWrap = TRUE), group = "World") %>%
+     # Layers Control
+     addLayersControl(
+       baseGroups = c("Default", "Topographical", "World"),
+       options = layersControlOptions(collapsed = FALSE)
+     )  %>% addPolylines(color = "#63CBD4", popup = ~LINENAME)
+   })
+   # Create a Data Table
+   output$datatable <- DT::renderDataTable({
+     subset(deathInput(), select = c(State, Cause.Name, Deaths, Age.adjusted.Death.Rate))
+   })
  }
  
  # Run the application 
